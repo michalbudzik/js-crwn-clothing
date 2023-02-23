aaa5a234acefeaba6f30d5304534181655e5c7bc
michalbudzikl <michal@mindborn.pl>
2023-02-23 13:29:05 +0100

    switched to hashrouter

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <UserProvider>
         <CategoriesProvider>
           <CartProvider>
@@ -19,7 +19,7 @@ root.render(
           </CartProvider>
         </CategoriesProvider>
       </UserProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
