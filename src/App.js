8f632a1058d847adf1693e534b0d2e76eeb3adba
michalbudzikl <michal@mindborn.pl>
2023-02-25 15:19:32 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Outlet } from "react-router-dom"; 
+import { Routes, Route, Outlet } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 import Authentication from "./routes/authentication/authentication.component";
@@ -8,11 +8,11 @@ import Checkout from "./routes/checkout/checkout.component";
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigation/>}>
-        <Route index element={<Home/>}/>
-        <Route path='shop/*' element={<Shop/>}/>
-        <Route path='auth' element={<Authentication/>}/>
-        <Route path='checkout' element={<Checkout/>}/>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop/*' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
