9760de3b28e6aedddbbc98c8570b7ccb3b015abc
michalbudzikl <michal@mindborn.pl>
2023-03-10 18:02:54 +0100

    cleanup

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -2,15 +2,15 @@ import { useContext, Fragment } from "react";
 import { CategoriesContext } from "../../contexts/categories.context";
 import CategoryPreview from "../../components/category-preview/category-preview.component";
 
-const  CategoriesPreview = () => {
-  const { categoriesMap} = useContext(CategoriesContext);
+const CategoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
   return (
     <Fragment>
       {
-          Object.keys(categoriesMap).map((title) => {
-            const products = categoriesMap[title];
-            return <CategoryPreview key={title} title={title} products={products}/>
-          })
+        Object.keys(categoriesMap).map((title) => {
+          const products = categoriesMap[title];
+          return <CategoryPreview key={title} title={title} products={products} />
+        })
       }
     </Fragment>
   )
